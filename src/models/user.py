53a94eadf984ec1eaf1f87a0f2d85faed49d01f4
Keshan Withanage <keshwithan@gmail.com>
2018-06-06 00:09:57 +0530

    Added login functions to the user model

--- src/models/user.py
+++ src/models/user.py
@@ -1,2 +1,40 @@
+import uuid
+from src.common.database import Database
+
+
 class User(object):
-    pass
+    def __init__(self, email, password, _id=None):
+        self.email = email
+        self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
+
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one("users", {"email": email})
+        if data is not None:
+            return cls(**data)
+
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one("users", {"_id": _id})
+        if data is not None:
+            return  cls(**data)
+
+    @staticmethod
+    def login_valid(email, password):
+        user = User.get_by_email(email)
+        if user is not None:
+            return  user.password == password
+        return  False
+
+    def register(self):
+        pass
+
+    def login(self):
+        pass
+
+    def json(self):
+        pass
+
+    def save_to_mongo(self):
+        pass
