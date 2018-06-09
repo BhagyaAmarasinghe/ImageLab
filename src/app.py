730273560b4706603f63d0ab7f9d41f1511caf4b
Keshan Withanage <keshwithan@gmail.com>
2018-06-10 02:42:53 +0530

    Added base.html to templates

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_method():
-    return "Hello from pycharm"
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
