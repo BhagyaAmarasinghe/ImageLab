b017f531cb3c90897a6425c3c8c576a07e4b9053
Keshan Withanage <keshwithan@gmail.com>
2018-06-23 08:32:02 +1000

    added project function to project.py

--- src/models/project.py
+++ src/models/project.py
@@ -1,2 +1,29 @@
+import uuid
+
+from flask import session
+
+from src.common.database import Database
+
 class Project(object):
-    pass
+     def __init__(self, projectId, projectName, projectDescription):
+        self.projectId = projectId
+        self.projectName = projectName
+        self._projectDescription = projectDescription
+
+    @classmethod
+    def get_project(cls, projectId):
+        data = Database.find_one("project", {"projectId": projectId})
+        if data is not None:
+            return cls(**data)
+
+    @staticmethod
+    def create_project(projectName, projectDescription):
+        projectId = get_project(projectId)
+        if projectId is not None:
+            return  project.projectId == projectId
+        return  False
+
+
+    def save_to_mongo(self):
+      print(self.json())
+      Database.insert("project", self.json())
